4c69a81c6838e34eaab8ba66b5b0422f7c737ad2
itsalineyall <avpaline@gmail.com>
2023-05-23 23:50:02 +0100

    First Commit

---
+++ printf/libfprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libfprintf.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvieira <alvieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 23:11:00 by alvieira          #+#    #+#             */
+/*   Updated: 2023/05/23 23:49:12 by alvieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFPRINTF_H
+# define LIBFPRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h> // va_list, va_start, va_arg, va_end
+
+
+
+#endif
